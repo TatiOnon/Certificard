e11bc0c7f1193031b06a150dc3af5d9dc6d6a32d
Tatiana Gasparini <91324879+TatiOnon@users.noreply.github.com>
2021-09-24 18:44:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mudaTema() {
+  document.body.classList.toggle("dark"); 
+}
+
+// P/ buscar emojis = bag emoji no Google 
+// P/ buscar fontes = google fontes
+
+//Desafios desta aula!
+//Terminar todo o design que está no figma (disponível //abaixo)
+//Adicionar suas redes sociais de forma que quem clique //consiga abrí-las
